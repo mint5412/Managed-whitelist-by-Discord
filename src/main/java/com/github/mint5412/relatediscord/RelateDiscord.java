7a7b18fa3acef32ac41fd48f55aa6d0595ef2968
mint5412 <atsushi20010520@gmail.com>
2022-12-26 00:36:12 +0900

    update

--- src/main/java/com/github/mint5412/relatediscord/RelateDiscord.java
+++ src/main/java/com/github/mint5412/relatediscord/RelateDiscord.java
@@ -1,17 +1,45 @@
 package com.github.mint5412.relatediscord;
 
+import com.github.mint5412.relatediscord.Systems.System;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 public final class RelateDiscord extends JavaPlugin {
 
+    private String token;
+    private JDA bot;
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        LoadToken();
+        try {
+            this.bot = JDABuilder.createDefault(this.token)
+                    .setRawEventsEnabled(true)
+                    .setActivity(Activity.playing("サーバーを監視中"))
+                    .build()
+            ;
+        } catch (Exception e) {
+            System.Error(e.getMessage());
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public void LoadToken() {
+        File tokenFile = new File("./BotToken.yml");
+        System.Error("token file is failed");
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(tokenFile);
+        this.token = config.getString("token");
+        if (this.token == null) System.Error("token is not setting");
+    }
+
+    public JDA getBot() {return this.bot;}
 }
---
+++ src/main/java/com/github/mint5412/relatediscord/Systems/System.java
@@ -0,0 +1,8 @@
+package com.github.mint5412.relatediscord.Systems;
+
+public class System {
+    public static void Error(String message) {
+        java.lang.System.out.println("Error: " + message);
+        java.lang.System.exit(0);
+    }
+}
