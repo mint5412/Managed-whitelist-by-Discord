c4bb62e5b3e5907a90b7cc4a9b86cb5060990b3b
mint5412 <atsushi20010520@gmail.com>
2022-12-25 23:54:50 +0900

    first commit

---
+++ src/main/java/com/github/mint5412/relatediscord/RelateDiscord.java
@@ -0,0 +1,17 @@
+package com.github.mint5412.relatediscord;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RelateDiscord extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
